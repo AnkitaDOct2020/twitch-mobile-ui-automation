2def5a9e11ba378e2280916abd5b06a67de35cf2
AnkitaD_Testing <73325289+AnkitaDOct2020@users.noreply.github.com>
2026-05-17 11:10:06 +0000

    Commit 13

--- src/test/java/pages/StreamerPage.java
+++ src/test/java/pages/StreamerPage.java
@@ -15,7 +15,7 @@ public class StreamerPage {
     public void selectFirstStreamer() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement streamer = wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("(//div[@data-a-target='search-result-live-channel'])[1]")
+            By.xpath("(//img[@class='tw-image'])[1]")
         ));
         streamer.click();
     }
